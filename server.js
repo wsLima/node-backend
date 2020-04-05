b0e42da309847c73b1187889c2902ab950c77236
Kennedy Lima <kennedy.wslima@gmail.com>
2020-04-05 10:19:56 -0400

    adicionando rotas diferentes.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer(function (req, resp) {
-    resp.end(`
+    let html = '';
+    if(req.url =='/') {
+        html = `
         <html>
             <head>
                 <meta charset="utf-8">
@@ -10,7 +12,21 @@ const server = http.createServer(function (req, resp) {
                 <h1> Olá Mundo </h1>
             </body> 
         </html>
-    `);
+    `; 
+    } else if (req.url == '/livros')
+    html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Listagem de livros </h1>
+            </body> 
+        </html>
+    `;
+    
+    resp.end(html);
 });
+
 server.listen(3000);
 
